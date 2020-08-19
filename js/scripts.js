9b4b9ed685d5513d13fb9ce7383d1ea45dd202d0
Harmony Trevena <harmonytrevena@gmail.com>
2020-08-19 12:14:43 -0400

    Initial Commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const myShoppingList = document.getElementById('myList');
+const inputItems = document.querySelectorAll('input');
+const myList = document.createElement('ul');
+myList.innerHTML = "My Shopping List:";
+myShoppingList.appendChild(myList); 
+
+function addItemsToList(inputItems, myList) {
+    inputItems.forEach(function (item) {
+        const itemName = item.value;
+        const itemList = document.createElement('li');
+        itemList.innerHTML = itemName;
+        myList.appendChild(itemList);
+    });
+};
+
+generateList.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    addItemsToList(inputItems, myList);
+});
+
